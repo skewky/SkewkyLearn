db7b34a6300baca2d71aa8e442d8cca879c47bf3
Bin Zhou <bin.zhou@bentley.com>
2021-03-12 01:39:55 +0800

    get all product info

--- GooglePlugin/BentleyWeb/background.js
+++ GooglePlugin/BentleyWeb/background.js
@@ -1,14 +1,25 @@
-  chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log('The color is green.');
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'www.baidu.com'},
-        })
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // 注册规则，当且仅当列出的所有条件都满足时，PageStateMatcher 才会匹配网页,即当url为https://www.zybang.com ，即触发执行某个操作（目前只有 ShowPageAction）。
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: '.baidu.com' },
+            // pageUrl: {hostEquals: 'www.baidu.com'},
+          })
         ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
+        // And shows the extension's page action.
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
   });
+
+
+  chrome.storage.sync.set({ color: '#3aa757' }, function () {
+    console.log('The color is green.');
+  });
+
+});
--- GooglePlugin/BentleyWeb/myscript.js
+++ GooglePlugin/BentleyWeb/myscript.js
@@ -6,9 +6,14 @@ function fireContentLoadedEvent () {
 
     // PUT YOUR CODE HERE.
     //document.body.textContent = "Changed this!
-	 var baidu=document.getElementById('su');
-	 baidu.value = "Baidu";
+	var baidu=document.getElementById('su');
+	if (baidu)
+		baidu.value = "Baidu";
 	
+	prodInfo = getAllProductStats();
+
+	//console.log(forShow);
+	//alert(forShow);
 	// Checking page title
 	
 	// 通过追加script脚本修改变量
@@ -21,3 +26,91 @@ function fireContentLoadedEvent () {
 	}
 
 }
+function getAllProductStats()
+{
+	var prodList = []
+	var alldivs = getClassObj('ZeroPadding ClearBoth FullWidth')
+	var forShow = ""
+	for( var i=0;i< alldivs.length;i++ ){
+		var prodInfo = {
+			    		name:"BentleyProduct",
+						version : "10.03.00.003",
+						stamp : "pp-vancouver",
+						time : "2021.12.23 12:24:35",
+						stat : "setup",
+						log : "d:/sfsf/sfsdf/sdf/sdf/sdf/sdf",
+						action: "rebuild",
+					};
+		//if(i==0)
+		{
+			//get action
+			var actionDiv = getClassObj('RightFloat FormContent RightAlignText',alldivs[i]);
+			if( actionDiv[0]) prodInfo.action = actionDiv[0].innerHTML
+			
+			//get name
+			var prodNameDiv = getClassObj('PRGFormText FormContent LeftFloat TextIndent',alldivs[i]);
+			if( prodNameDiv[0]) prodInfo.name = prodNameDiv[0].innerHTML
+
+            //get info
+			var prodInfoDiv = getClassObj('PRGFormText ClearBoth LeftFloat FullWidth',alldivs[i]);
+			if( prodInfoDiv[0]) prodInfo.version = prodInfoDiv[0].textContent;
+			if( prodInfoDiv[1]) prodInfo.stamp = prodInfoDiv[1].textContent;
+			if( prodInfoDiv[2]) prodInfo.time =  prodInfoDiv[2].textContent;
+			if( prodInfoDiv[3]) prodInfo.stat = prodInfoDiv[3].textContent;
+			if( prodInfoDiv[4]) prodInfo.log =  prodInfoDiv[4].textContent;
+
+			prodInfo.version = prodInfo.version.replace("Version: ","");
+			prodInfo.stamp = prodInfo.stamp.replace("Stamp: ","");
+			prodInfo.time =  prodInfo.time.replace("Start Time: ","");
+			prodInfo.stat = prodInfo.stat.replace("Current Status: ","");
+			prodInfo.log = prodInfo.log.replace("LogFile: .","");
+			prodInfo.log = prodInfo.log.replace(" (FTP)","");
+			
+			var msg = i+" name="+prodInfo.name+ 
+					    "\t version="+prodInfo.version+
+						"\t stamp="+prodInfo.stamp+
+						"\t time="+prodInfo.time+
+						"\t stat="+prodInfo.stat+
+						"\t log="+prodInfo.log//+
+						//" Action="+prodInfo.action
+			console.log (msg);
+
+			var myDiv = document.createElement("myDiv"); 
+
+			var newContent = document.createTextNode("Hi there and greetings!"); 
+
+			myDiv.appendChild(newContent); 
+
+			var currentDiv = document.getElementById("frmMain"); 
+
+			document.body.insertBefore(myDiv, currentDiv); 
+
+			//console.log (i+" name="+prodInfo.name+"\t version="+prodInfo.version+"\t stamp="+prodInfo.stamp+"\t time="+prodInfo.time+"\t stat="+prodInfo.stat+"\t log="+prodInfo.log+" Action="+prodInfo.action);
+			
+			prodList.push(prodInfo);
+		}
+		forShow = forShow + alldivs[i].innerHTML;
+	}
+	return prodList;
+}
+
+function getClassObj(className, tag){
+    tag = tag||document;
+    className = className||'*';
+    //console.log ("ClassName="+className);
+    //console.log ("Tag="+tag);
+    var findarr = [];
+    if(tag.getElementsByClassName){
+        return tag.getElementsByClassName(className);
+    }
+	//console.log ("getElementsByClassName faild");
+    el = tag.getElementsByTagName(className);
+    var pattern = new RegExp("(^|\\s)" + className + "(\\s|$)");
+    for(var i=0; i<el.length; i++){
+        if(pattern.test(el[i].className)){
+            findarr.push(el[i]);
+        }
+    }
+	return findarr;
+}
+   
